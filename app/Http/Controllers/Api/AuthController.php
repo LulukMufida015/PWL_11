be242e4091e086f2df0650260f065699f3a85faa
Luluk Mufida <lulukmufida0015@gmail.com>
2021-04-20 18:29:30 +0700

    Menambahkan function  register  dengan  parameter  RegisterRequest  pada Api/AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
+use App\Http\Requests\RegisterRequest;
+use Auth;
+use Symfony\Component\HttpFoundation\Response as Response;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AuthController extends Controller
 {
     Use ApiResponse;
+    public function register(RegisterRequest $request)
+    {
+    	$validated = $request->validated();
+    	$user = User::create([
+    		'name'=>$validated['name'],
+    		'email'=>$validated['email'],
+    		'password'=>bcrypt($validated['password']),
+    	]);
+
+    	$token = $user->createToken('auth_token')->plainTextToken;
+    	return $this->apiSuccess([
+    		'token'=>$token,
+    		'token_type'=>'Bearer',
+    		'user'=>$user,
+    	]);
+    }
 }
